eee2f873541021fe6926465298dd5bbbc4d019b4
hsilman <harry.silman@gmail.com>
2017-04-28 18:58:44 -0400

    Create mario.py

---
+++ mario.py
@@ -0,0 +1,16 @@
+import cs50
+
+def main():
+    print ("Height: ", end="") 
+    height = cs50.get_int()
+    
+    if height > 23 or height < 0:
+        print("Retry: ", end="")
+        height = cs50.get_int()
+    
+    for i in range(height+1):
+        print(' '*(height - i) + '#'*i + '  ' + '#'*i)
+        
+        
+if __name__ == '__main__':
+    main()
